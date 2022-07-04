3758de621ad028dda8ec70e9f75eb675ab0e2067
Eric Domke <erdomke@gmail.com>
2022-07-04 15:07:28 -0400

    Improve support for airbnb

--- source/background.js
+++ source/background.js
@@ -2,18 +2,7 @@
 import './options-storage.js';
 import browser from 'webextension-polyfill';
 
-/**
- * @param {Record<string, any>} object 
- * @param {string[]} path 
- */
-const ensurePath = (object, path) => {
-  let curr = object;
-  for (let i = 0; i < path.length; i++) {
-    let next = curr[path[i]] ?? {};
-    curr[path[i]] = next;
-    curr = next;
-  }
-}
+// https://github.com/zotero/translate
 
 /**
  * @param {Record<string, any>} object 
@@ -27,22 +16,48 @@ const byPartialKey = (object, keySubstring) => {
   return undefined;
 }
 
+const processAirbnbState = (state) => {
+  try {
+    let root = state.niobeMinimalClientData[0][1].data.presentation;
+    console.log("Airbnb processing");
+    let groups = {};
+    groupAirbnbByType(root, groups);
+    console.log(groups);
+    return groups;
+  } catch (err) {
+    return null;
+  }
+}
+
+const groupAirbnbByType = (current, groups) => {
+  let typeName = current["__typename"] ?? "unknown";
+  let list = groups[typeName] ?? [];
+  groups[typeName] = list;
+  list.push(current);
+  for (let key in current) {
+    if (current[key] && current[key]["__typename"]) {
+      groupAirbnbByType(current[key], groups);
+    } else if (Array.isArray(current[key])) {
+      for (let child of current[key].filter(c => c && c["__typename"]))
+        groupAirbnbByType(child, groups);
+    }
+  } 
+}
+
 browser.runtime.onMessage.addListener(async (message, sender) => {
   if ('start' === message) {
     let result = {};
     try {
       let tab = (await browser.tabs.query({ active: true, currentWindow: true }))[0];
-      result.icon = tab.favIconUrl;
-      result.title = tab.title;
-      result.url = tab.url;
-
       let page = {};    
 
       let scriptResults = await browser.scripting.executeScript({
         target: { tabId: tab.id},
         func: () => {
-          let metaObject = {
-            "html_title": document.head.querySelector("title").textContent 
+          let result = {
+            html: document.documentElement.outerHTML,
+            title: document.head.querySelector("title").textContent,
+            meta: {}
           };
           for (let meta of document.head.querySelectorAll('meta')) {
             const property = meta.getAttribute('property') 
@@ -51,71 +66,114 @@ browser.runtime.onMessage.addListener(async (message, sender) => {
               || meta.getAttribute('charset');
             const content = meta.getAttribute("content");
             if (property && content)
-			        metaObject[property.toLowerCase()] = content;
+			        result.meta[property.toLowerCase()] = content;
           }
-
-          let schema = document.querySelector('script[type="application/ld+json"]');
-          if (schema)
-            metaObject.schema = JSON.parse(schema.innerHTML);
-          else
-            metaObject.schema = {};
 
           let urlLink = document.querySelector('link[rel="canonical"]')
             || document.querySelector('link[rel="alternate"][hreflang="x-default"]');
           if (urlLink)
-            metaObject.url = urlLink.getAttribute("href");
+            result.url = urlLink.getAttribute("href");
 
-          return {
-            meta: metaObject,
-            html: document.documentElement.outerHTML
-          };
+          let schema = document.querySelector('script[type="application/ld+json"]');
+          if (schema)
+            result.schema = JSON.parse(schema.innerHTML);
+          else
+            result.schema = {};
+
+          // Airbnb
+          let airbnbState = document.querySelector('#data-deferred-state[type="application/json"]');
+          if (airbnbState)
+            result.airbnbState = JSON.parse(airbnbState.innerHTML);
+          return result;
         }
       })
       page = scriptResults[0].result;
 
-      //result.meta = page.meta;
-      ensurePath(page.meta.schema, ["author"]);
-      ensurePath(page.meta.schema, ["brand"]);
-  
-      result.author = page.meta.schema.author.name
-        || page.meta.schema.brand.name
-        || page.meta.author
-        || page.meta["article:author"];
-      result.date = page.meta.schema.dateModified
-        || page.meta.schema.datePublished
-        || page.meta.schema.dateCreated
-        || byPartialKey(page.meta, "updated_time")
-        || byPartialKey(page.meta, "modified_time")
-        || byPartialKey(page.meta, "published_time")
-        || byPartialKey(page.meta, "release_date")
-        || page.meta["date"]
-        || byPartialKey(page.meta, "dc.date")
-        || byPartialKey(page.meta, "dcterms.date")
-        || byPartialKey(page.meta, "dc:date")
-        || byPartialKey(page.meta, "dc:created");
-      result.description = page.meta["og:description"] 
-        || page.meta["twitter:description"]
-        || page.meta.description
-        || page.meta.schema.articleBody
-        || page.meta.schema.description;
-      result.image = page.meta["og:image:secure_url"] 
-        || page.meta["og:image:url"]
-        || page.meta["og:image"]
-        || page.meta["twitter:image:src"]
-        || page.meta["twitter:image"]
-        || page.meta["image"]
-        || ((page.meta.schema.image ?? [])[0] ?? {}).url
-        || (page.meta.schema.image ?? {}).url
-        || page.meta.schema.image;
-      result.title = page.meta["og:title"] 
-        || page.meta["twitter:title"]
-        || page.meta.html_title
-        || page.meta.schema.headline
-        || result.title;
-      result.url = page.meta["og:url"]
-        || page.meta["twitter:url"]
-        || page.meta.url
-        || result.url;
+      Object.assign(result, page.schema);
+
+      if (!result.author && !result.brand) {
+        let authorName = page.meta.author
+          || page.meta["article:author"];
+        if (authorName)
+          result.author = {
+            name: authorName
+          };
+      }
+      if (!result.dateCreated) {
+        result.dateCreated = byPartialKey(page.meta, "dc:created");
+      }
+      if (!result.dateModified) {
+        result.dateModified = byPartialKey(page.meta, "updated_time")
+          || byPartialKey(page.meta, "modified_time")
+          || page.meta["date"]
+          || byPartialKey(page.meta, "dc.date")
+          || byPartialKey(page.meta, "dcterms.date")
+          || byPartialKey(page.meta, "dc:date")
+      }
+      if (!result.datePublished) {
+        result.datePublished = byPartialKey(page.meta, "published_time")
+          || byPartialKey(page.meta, "release_date")
+      }
+      if (!result.description) {
+        result.description = page.meta["og:description"] 
+          || page.meta["twitter:description"]
+          || page.meta.description;
+      }
+      result.icon = tab.favIconUrl;
+      if (!result.image) {
+        result.image = page.meta["og:image:secure_url"] 
+          || page.meta["og:image:url"]
+          || page.meta["og:image"]
+          || page.meta["twitter:image:src"]
+          || page.meta["twitter:image"]
+          || page.meta["image"];
+      }
+      if (!result.name) {
+        result.name = page.meta["og:title"] 
+          || page.meta["twitter:title"]
+          || page.title
+          || tab.title;
+      }
+      if (!result.url) {
+        result.url = page.meta["og:url"]
+          || page.meta["twitter:url"]
+          || page.url
+          || tab.url;
+      }
+
+      if (page.airbnbState) {
+        result["@context"] = "http://schema.org";
+        result["@type"] = result["@type"] ?? "BedAndBreakfast";
+
+        let airbnbGroups = processAirbnbState(page.airbnbState);
+        let location = (airbnbGroups["LocationSection"] ?? [])[0] ?? {};
+        result.latitude = location.lat;
+        result.longitude = location.lng;
+        result.address = {
+          "streetAddress": location.subtitle
+        }
+
+        let policies = (airbnbGroups["PoliciesSection"] ?? [])[0] ?? { };
+        result.additionalHouseRules = policies.additionalHouseRules;
+        if (policies.houseRules) {
+          result.houseRules = policies.houseRules.map(r => r.title);
+        }
+
+        let overview = (airbnbGroups["PdpOverviewSection"] ?? [])[0] ?? { };
+        if (overview.detailItems) {
+          result.amenityFeature = overview.detailItems.map(i => ({"value": i.title}));
+        }
+
+        let sharingConfig = (airbnbGroups["PdpSharingConfig"] ?? [])[0] ?? { };
+        result.personCapacity = sharingConfig.personCapacity;
+        result.propertyType = sharingConfig.propertyType;
+        result.aggregateRating = {
+          "@type": "AggregateRating",
+          "bestRating": 5,
+          "ratingValue": sharingConfig.starRating,
+          "reviewCount": sharingConfig.reviewCount
+        }
+      }
     } catch (err) {
       result.error = JSON.parse(JSON.stringify(err, Object.getOwnPropertyNames(err)));
     }
